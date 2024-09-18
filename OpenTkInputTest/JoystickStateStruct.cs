168bff0624101208bff917863e0fb54f66b45d08
lemming104 <chrisaelwell@gmail.com>
2024-09-17 19:10:38 -0700

    Add simple demo project

---
+++ OpenTkInputTest/JoystickStateStruct.cs
@@ -0,0 +1,39 @@
+﻿namespace OpenTkInputTest
+{
+    using OpenTK.Windowing.Common.Input;
+    using OpenTK.Windowing.GraphicsLibraryFramework;
+
+    public struct JoystickStateStruct
+    {
+        public readonly int JoystickId;
+        public readonly float[] AxisValues;
+        public readonly bool[] ButtonStates;
+        public readonly Hat[] HatPositions;
+
+        public JoystickStateStruct(JoystickState state)
+        {
+            JoystickId = state.Id;
+            AxisValues = new float[state.AxisCount];
+            ButtonStates = new bool[state.ButtonCount];
+            HatPositions = new Hat[state.HatCount];
+
+            Update(state);
+        }
+
+        public void Update(JoystickState state)
+        {
+            for (int axisId = 0; axisId < AxisValues.Length; axisId++)
+            {
+                AxisValues[axisId] = state.GetAxis(axisId);
+            }
+            for (int buttonId = 0; buttonId < ButtonStates.Length; buttonId++)
+            {
+                ButtonStates[buttonId] = state.IsButtonDown(buttonId);
+            }
+            for (int hatId = 0; hatId < HatPositions.Length; hatId++)
+            {
+                HatPositions[hatId] = state.GetHat(hatId);
+            }
+        }
+    }
+}
---
+++ OpenTkInputTest/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace OpenTkInputTest
+{
+    using OpenTK.Windowing.Desktop;
+    using System;
+
+    public class Program
+    {
+        private readonly GameWindow m_window;
+        JoystickAdapter m_joystickAdapter;
+
+        static void Main()
+        {
+            GameWindow window = new GameWindow(new() { UpdateFrequency = 144, RenderFrequency = 144 }, new() { Size = (800, 600) });
+            Program program = new Program(window);
+            window.Run();
+        }
+
+        public Program(GameWindow window)
+        {
+            m_window = window;
+            m_window.UpdateFrame += Window_UpdateFrame;
+            m_window.JoystickConnected += this.M_window_JoystickConnected;
+            m_joystickAdapter = new JoystickAdapter(window.JoystickStates, 0.2f);
+
+            m_joystickAdapter.ButtonPressed += this.M_joystickAdapter_ButtonPressed;
+            m_joystickAdapter.AxisMoved += this.M_joystickAdapter_AxisMoved;
+            m_joystickAdapter.HatMoved += this.M_joystickAdapter_HatMoved;
+        }
+
+        private void M_window_JoystickConnected(OpenTK.Windowing.Common.JoystickEventArgs obj)
+        {
+            m_joystickAdapter.RedetectJoysticks();
+        }
+
+        private void M_joystickAdapter_HatMoved(object source, HatEventArgs args)
+        {
+            Console.WriteLine($"Joystick: {args.JoystickId}, Hat: {args.HatId}, Direction: {args.HatDirection}");
+        }
+
+        private void M_joystickAdapter_AxisMoved(object source, AxisEventArgs args)
+        {
+            Console.WriteLine($"Joystick: {args.JoystickId}, Axis: {args.AxisId}, Position: {args.AbsolutePosition}, Relative: {args.DeltaPosition}, Corrected: {args.CorrectedPosition}");
+        }
+
+        private void M_joystickAdapter_ButtonPressed(object source, ButtonEventArgs args)
+        {
+            Console.WriteLine($"Joystick: {args.JoystickId}, Button: {args.ButtonId}, State: {(args.IsPressed ? "Pressed" : "Released")}");
+        }
+
+        private void Window_UpdateFrame(OpenTK.Windowing.Common.FrameEventArgs obj)
+        {
+            m_joystickAdapter.SampleJoystickStates();
+        }
+    }
+}
